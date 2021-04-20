2fd9118d30bc4db014f64bc0c4f43271cd5895c6
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-20 19:42:10 +0200

    Inicio del proyecto

---
+++ src/main/java/Hibernate_01_SergioAlonso/Hibernate_01_SergioAlonso/App.java
@@ -0,0 +1,13 @@
+package Hibernate_01_SergioAlonso.Hibernate_01_SergioAlonso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Hibernate_01_SergioAlonso/Hibernate_01_SergioAlonso/AppTest.java
@@ -0,0 +1,20 @@
+package Hibernate_01_SergioAlonso.Hibernate_01_SergioAlonso;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
