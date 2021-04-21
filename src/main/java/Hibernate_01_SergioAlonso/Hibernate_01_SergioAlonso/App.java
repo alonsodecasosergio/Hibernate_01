bd49fcddfcead1bd5990f87a266bf3478993f9ea
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-21 17:27:47 +0200

    Ficheros actualizados y funcionalidad del mapeo realizada

--- src/main/java/Hibernate_01_SergioAlonso/Hibernate_01_SergioAlonso/App.java
+++ src/main/java/Hibernate_01_SergioAlonso/Hibernate_01_SergioAlonso/App.java
@@ -1,13 +1,36 @@
 package Hibernate_01_SergioAlonso.Hibernate_01_SergioAlonso;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import dataModelDAO.EmpleadoDAO;
+import dataModelEntities.Empleado;
+import dataModelUtils.HibernateUtil;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Logger logger = LogManager.getLogger(App.class);
+	
+	static SessionFactory sessionFactory;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        
+        Empleado emple = EmpleadoDAO.getEmpleado(session, 1);
+        
+        System.out.println(emple.getNombre());
+        
+        EmpleadoDAO.insertEmpleados(session, new Empleado(10, "Sergio", "Alonso", "De Caso", "Zamora", 'a', "a", "a", "a", 1));
+        
+        System.out.println("FIN");
     }
 }
